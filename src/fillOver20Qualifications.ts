76982ab74145e4d8dc8fcb76f27732f11721ac7a
Yuichi Suzuki <ys.development0601@gmail.com>
2022-11-01 15:12:46 +0900

    主処理を別ファイルに分ける

---
+++ src/fillOver20Qualifications.ts
@@ -0,0 +1,47 @@
+import { By, ThenableWebDriver } from 'selenium-webdriver';
+
+import { sleep } from '../libs/systems';
+
+const sikCds = [
+  '115970002',
+  '115970003',
+  '115980000',
+  '121080001',
+  '121080002',
+  '125300010',
+  '125300020',
+  '125301130',
+  '125301140',
+  '125310001',
+  '125310002',
+  '125310010',
+  '125310011',
+  '125310012',
+  '125310013',
+  '125310014',
+  '125310015',
+  '125310021',
+  '125310022',
+  '125310023',
+  '125310024',
+  '125310025',
+  '125310026',
+  '125310027',
+  '125330001',
+];
+
+export const execute = async (driver: ThenableWebDriver) => {
+  await driver.findElement(By.partialLinkText('技能資格申請')).click();
+  await driver.findElement(By.partialLinkText('技能資格申請')).click();
+
+  await sleep(1);
+
+  for (let i = 0; i < sikCds.length; ++i) {
+    await driver.findElement(By.css('input[name="@SUB.ADD."')).click();
+    await driver.findElement(By.name('sikaku_sik_cd')).sendKeys(sikCds[i]);
+    await driver.findElement(By.css('input[name="@SUB.NEXT.null"')).click();
+    await driver.findElement(By.css('input[name="@SUB.DECIDE."')).click();
+  }
+};
+
+export default execute;
--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,18 @@
-import { By, Key, ThenableWebDriver, until } from 'selenium-webdriver';
+import { By, Key, ThenableWebDriver } from 'selenium-webdriver';
 
-import { sleep } from '../libs/systems';
+// const URL = 'http://localhost:8080/conversion-nbh-cws/cws/cws';
+const URL = 'https://tmeva-develop-cws.company.lab.works-hi.com/self-workflow/cws/cws';
+const USERID = 'cws0001';
 
-const sikCds = [
-  '115970002',
-  '115970003',
-  '115980000',
-  '121080001',
-  '121080002',
-  '125300010',
-  '125300020',
-  '125301130',
-  '125301140',
-  '125310001',
-  '125310002',
-  '125310010',
-  '125310011',
-  '125310012',
-  '125310013',
-  '125310014',
-  '125310015',
-  '125310021',
-  '125310022',
-  '125310023',
-  '125310024',
-  '125310025',
-  '125310026',
-  '125310027',
-  '125330001',
-];
+const processFile = './fillOver20Qualifications.ts';
 
 export const main = async (driver: ThenableWebDriver) => {
   // ↓↓↓ any process you wants.
-  await driver.get(`http://localhost:8080/conversion-nbh-cws/cws/cws`);
+  await driver.get(URL);
+  await driver.findElement(By.name('uid')).sendKeys(USERID, Key.ENTER);
 
-  await driver.findElement(By.name('uid')).sendKeys('cws0001', Key.ENTER);
-  await driver.findElement(By.partialLinkText('技能資格申請')).click();
-  await driver.findElement(By.partialLinkText('技能資格申請')).click();
-
-  await sleep(1);
-
-  for (let i = 0; i < sikCds.length; ++i) {
-    await driver.findElement(By.css('input[name="@SUB.ADD."')).click();
-    await driver.findElement(By.name('sikaku_sik_cd')).sendKeys(sikCds[i]);
-    await driver.findElement(By.css('input[name="@SUB.NEXT.null"')).click();
-    await driver.findElement(By.css('input[name="@SUB.DECIDE."')).click();
-  }
+  const process = await import(processFile);
+  await process.execute(driver);
 
   // await driver.quit();
   // ↑↑↑ any process you wants.
