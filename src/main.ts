97c45cb12c71f066048a241a8aefff39a72208ea
Yuichi Suzuki <ys.development0601@gmail.com>
2022-11-01 15:19:54 +0900

    微修正

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,19 @@ import { By, Key, ThenableWebDriver } from 'selenium-webdriver';
 // const URL = 'http://localhost:8080/conversion-nbh-cws/cws/cws';
 const URL = 'https://tmeva-develop-cws.company.lab.works-hi.com/self-workflow/cws/cws';
 const USERID = 'cws0001';
+const PASSWORD = '';
 
 const processFile = './fillOver20Qualifications.ts';
 
 export const main = async (driver: ThenableWebDriver) => {
-  // ↓↓↓ any process you wants.
   await driver.get(URL);
-  await driver.findElement(By.name('uid')).sendKeys(USERID, Key.ENTER);
+  await driver.findElement(By.name('uid')).sendKeys(USERID);
+  await driver.findElement(By.name('pwd')).sendKeys(PASSWORD, Key.ENTER);
 
   const process = await import(processFile);
   await process.execute(driver);
 
   // await driver.quit();
-  // ↑↑↑ any process you wants.
 };
 
 export default main;
